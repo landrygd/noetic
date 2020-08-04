39ce509b884ecf28ea30ad9e5f1785c70b6303ae
Landry <landry.valetoux.lv@gmail.com>
2020-08-04 10:55:34 +0200

    compiler update

--- ionic/compiler.bat
+++ ionic/compiler.bat
@@ -2,9 +2,7 @@ ionic cordova build --release android
 cd .\platforms\android
 ./gradlew.bat bundle
 cd .\platforms\android\app\build\outputs\bundle\release
-copy ..\..\keystores\noetic.keystore C:\Users\GAMING\Projets\noetic\ionic\platforms\android\app\build\outputs\bundle\release\noetic.keystore
-echo 9pZC3F4dn
-echo wAf8fA36A
+copy ..\..\keystores\noetic.keystore .\android\app\build\outputs\bundle\release\noetic.keystore
 jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore noetic.keystore app.aab noetic
 zipalign -v 4 app.aab noetic.aab
 explorer .\platforms\android\app\build\outputs\bundle\release
