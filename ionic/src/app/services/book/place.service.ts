57d7abd45168c993175d6fecc2fdcd336a51e415
Landry <landry.valetoux.lv@gmail.com>
2020-08-27 12:20:52 +0200

    maj

--- ionic/src/app/services/book/place.service.ts
+++ ionic/src/app/services/book/place.service.ts
@@ -1,8 +1,10 @@
+import { BookService } from 'src/app/services/book.service';
 import { PopupService } from 'src/app/services/popup.service';
 import { TranslateService } from '@ngx-translate/core';
 import { Subscription } from 'rxjs';
 import { AlertController } from '@ionic/angular';
 import { Injectable } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/firestore';
 
 @Injectable({
   providedIn: 'root'
@@ -20,7 +22,9 @@ export class PlaceService {
   constructor(
     public translator: TranslateService,
     private alertController: AlertController,
-    private popupService: PopupService
+    private popupService: PopupService,
+    private firestore: AngularFirestore,
+    private bookService: BookService
     ) {
       this.getTraduction();
     }
@@ -37,8 +41,9 @@ export class PlaceService {
     });
   }
 
-  addActor(id: string, name: string) {
-
+  addPlace(id: string, name: string) {
+    const place = {id, name};
+    this.firestore.collection('/books').doc(this.bookService.curBookId).collection('places').doc(place.id).set(place);
   }
 
   async newPlace() {
